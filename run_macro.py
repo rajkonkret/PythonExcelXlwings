42a0d5f9876e616513a93bd427f9b6623e3063b0
radek <rajkonkret660@gmail.com>
2025-08-11 18:38:38 +0000

    add plik_z_makrem

--- run_macro.py
+++ run_macro.py
@@ -4,10 +4,10 @@ import xlwings as xw, psutil
 wb = xw.Book()
 
 # Import modułu VBA
-wb.api.VBProject.VBComponents.Import(r"makro.bas")
+wb.api.VBProject.VBComponents.Import(r"C:\Users\radek\PycharmProjects\PythonExcelXlwings\makro.bas")
 
 # Zapisz jako plik z makrami (.xlsm)
-wb.save(r"plik_z_makrem.xlsm")
+wb.save(r"C:\Users\radek\PycharmProjects\PythonExcelXlwings\plik_z_makrem.xlsm")
 
 # Uruchom makro
 makro = wb.macro("HelloWorld")
